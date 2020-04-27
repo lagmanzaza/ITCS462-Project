7bf26d7e769529d3effa72d97d45f136ea61c0b2
jarupong pajakgo <lagmanmc@gmail.com>
2020-04-27 10:53:10 +0700

    [Fucking Completegit add .git add .git add .git add .git add .git add .]

--- frontend/src/components/party-card.js
+++ frontend/src/components/party-card.js
@@ -1,13 +1,13 @@
 import React from "react";
 // style={{ width: 20 + "em" }}
 // <img src="https://picsum.photos/768" alt="ss" />
-export default ({ name, description, score, id, onClick, setVoteMessage }) => (
+export default ({ name, description, score, id, onClick }) => (
   <div className="card">
     <div className="card-body" style={{ textAlign: "center" }}>
       <h4 className="card-title">Party Name: {name}</h4>
       <p className="card-text">Description: {description}</p>
       <p className="card-text">Score: {score}</p>
-      <button onClick={() => onClick(setVoteMessage, id)}>Vote</button>
+      <button onClick={() => onClick(id)}>Vote</button>
     </div>
   </div>
 );
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,28 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import PartyCard from "../components/party-card";
 import { withRouter } from "react-router-dom";
 import getPartiesService from "../services/parties/get-query";
 import voteService from "../services/votes/create";
-import socketIOClient from "socket.io-client";
 import checkUserVoteService from "../services/votes/check-user";
-
-const ENDPOINT = "http://127.0.0.1:3030";
-
-const onVote = async (setVotedMessage, partyId) => {
-  try {
-    await voteService(partyId);
-    setVotedMessage({
-      message: `you voted`,
-      isVoted: true,
-    });
-  } catch (e) {
-    console.log(e);
-    setVotedMessage({
-      message: `you vote`,
-      isVoted: true,
-    });
-  }
-};
+import socket from "../services/socket";
 
 const VoteFlagMessage = ({ message, isvote }) => {
   if (isvote) {
@@ -32,71 +14,102 @@ const VoteFlagMessage = ({ message, isvote }) => {
   return <h2 style={{ textAlign: "center" }}>{message}</h2>;
 };
 
-/* eslint-disable */
-export default withRouter((props) => {
-  const [listParties, setParties] = useState([]);
-  const [votedMessage, setVotedMessage] = useState({
-    message: `you don't vote`,
-    isVoted: false,
-  });
-  useEffect(() => {
-    const socket = socketIOClient(ENDPOINT);
-    socket.on("on vote", (data) => {
-      setParties((parties) => {
-        const partyIndex = parties.map((party) => party.id).indexOf(data.id);
-        parties[partyIndex].score = data.score;
-        return parties;
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listParties: [],
+      votedMessage: {
+        message: `you don't vote`,
+        isVoted: false,
+      },
+    };
+
+    this.onVote = this.onVote.bind(this);
+  }
+
+  componentDidMount() {
+    checkUserVoteService().catch(() => {
+      this.setState((prev) => {
+        prev["votedMessage"] = { message: `you voted`, isVoted: true };
+        return prev;
       });
     });
 
-    const fetchCheckUser = async () => {
-      try {
-        await checkUserVoteService();
-      } catch (e) {
-        setVotedMessage({
-          message: `you voted`,
-          isVoted: true,
-        });
-      }
-    };
+    socket.on("on vote", (data) => {
+      this.setState((prev) => {
+        const partyIndex = prev.listParties
+          .map((party) => party.id)
+          .indexOf(data.id);
+        prev.listParties[partyIndex].score = data.score;
+        return prev;
+      });
+    });
 
     const fetchParties = async () => {
       try {
         const result = await getPartiesService();
-        setParties(result.data);
+        this.setState((prev) => {
+          prev["listParties"] = result.data;
+          return prev;
+        });
         return result.data;
       } catch (e) {
         console.log(e.response);
-        props.history.push("/");
+        this.props.history.push("/");
       }
     };
 
-    fetchCheckUser().then(() => {
-      fetchParties();
-    });
-  }, []);
-  return (
-    <div>
-      <VoteFlagMessage
-        message={votedMessage.message}
-        isvote={votedMessage.isVoted}
-      />
-      <div className="row">
-        {listParties.map(({ name, description, id, score }, index) => {
-          return (
-            <div className="col-4 col" key={index}>
-              <PartyCard
-                name={name}
-                description={description}
-                id={id}
-                score={score}
-                onClick={onVote}
-                setVoteMessage={setVotedMessage}
-              />
-            </div>
-          );
-        })}
+    fetchParties();
+  }
+
+  async onVote(partyId) {
+    try {
+      await voteService(partyId);
+      this.setState((prev) => {
+        prev["votedMessage"] = {
+          message: `you voted`,
+          isVoted: true,
+        };
+        return prev;
+      });
+    } catch (e) {
+      console.log(e);
+      this.setState((prev) => {
+        prev["votedMessage"] = {
+          message: `you voted`,
+          isVoted: true,
+        };
+      });
+    }
+  }
+
+  render() {
+    const { votedMessage, listParties } = this.state;
+    return (
+      <div>
+        <VoteFlagMessage
+          message={votedMessage.message}
+          isvote={votedMessage.isVoted}
+        />
+        <div className="row" style={{ justifyContent: "center" }}>
+          {listParties.map(({ name, description, id, score }, index) => {
+            return (
+              <div className="sm-12 md-4 lg-4 col" key={index}>
+                <PartyCard
+                  name={name}
+                  description={description}
+                  id={id}
+                  score={score}
+                  onClick={this.onVote}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
-  );
-});
+    );
+  }
+}
+
+export default withRouter(IndexPage);
